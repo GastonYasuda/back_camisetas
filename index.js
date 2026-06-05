3ed8e157fe1bcee6687749c0d3636aeb7be67ca7
GastonYasuda <gastonyasuda@gmail.com>
2026-06-04 23:15:36 -0300

    .

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const express = require("express");
+const pool = require("./db");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("API funcionando");
+});
+
+app.get("/camisetas", async (req, res) => {
+    try {
+        const resultado = await pool.query(
+            "SELECT * FROM public.camisetas"
+        );
+
+        res.json(resultado.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: "Error al consultar la base"
+        });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en puerto ${PORT}`);
+});
