24b5953d168cdf669ce4e9d9df51451ed5990b59
GastonYasuda <gastonyasuda@gmail.com>
2026-06-08 12:51:29 -0300

    .

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ const pool = require("./db");
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en puerto ${PORT}`);
+});
+
+
+
 app.get("/", (req, res) => {
-    res.send("API funcionando");
+    res.status(200).send({ message: "API funcionando" });
 });
 
 
@@ -20,30 +27,25 @@ app.get("/camisetas", async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).json({
-            error: "Error al consultar la base"
+            error: "Error al consultar la baseeee"
         });
     }
 });
 
-app.get("/camisetas/:id_camiseta", (req, res) => {
-    const id = req.params.id_camiseta;
-    console.log('estoy andando');
+// app.get("/camisetas/:id_camiseta", (req, res) => {
+//     const id = req.params.id;
+//     console.log('estoy andando');
 
-    if (id !== '') {
-        const camisetaEncontrada = camisetas.find((camiseta) => {
-            camiseta.id_camiseta === id
-        })
-        if (camisetaEncontrada) {
-            res.status(200), json(camisetaEncontrada)
-        } else {
-            res.status(404).json({ message: "Camiseta no encontrada" })
-        }
+//     if (id !== '') {
+//         const camisetaEncontrada = camisetas.find((camiseta) => {
+//             camiseta.id_camiseta === id
+//         })
+//         if (camisetaEncontrada) {
+//             res.status(200), json(camisetaEncontrada)
+//         } else {
+//             res.status(404).json({ message: "Camiseta no encontrada" })
+//         }
 
-    }
-})
+//     }
+// })
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Servidor iniciado en puerto ${PORT}`);
-});
